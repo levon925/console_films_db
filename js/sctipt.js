f046284edb4eb4d0368539ab88562c18adb5d800
Levon <levon16.01.99@gmail.com>
2022-02-10 22:24:56 +0400

    personal movie db

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,3 +1,11 @@
 let numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
 
 console.log(numberOfFilms);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
