fe6f6f07df4126c19daf4b7985c1e3f67eec48ef
Levon <levon16.01.99@gmail.com>
2022-02-10 22:48:04 +0400

    answers and grade

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,7 +1,5 @@
 let numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
 
-console.log(numberOfFilms);
-
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,3 +7,13 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 };
+
+let a = prompt("Один из последних просмотренных фильмов?"),
+    b = prompt("На сколько оцените его?"),
+    c = prompt("Один из последних просмотренных фильмов?"),
+    d = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
